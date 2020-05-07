5459c805c6751c9b685ffa593dc41ba7ed63ec4a
fradam95 <fradam95@users.noreply.github.com>
2020-05-07 21:50:36 +0200

    _Marci_Adam_nagy_haladas_

--- Pages/forms.py
+++ Pages/forms.py
@@ -5,3 +5,12 @@ class ContactForm(forms.Form):
     dim2 = forms.IntegerField(max_value=10000)
     dim3 = forms.IntegerField(max_value=10000)
     message = forms.CharField(widget=forms.Textarea, max_length=1000)
+
+class ContactForm_Adam(forms.Form):
+    dim1 = forms.IntegerField(max_value=10000)
+    dim2 = forms.IntegerField(max_value=10000)
+    dim3 = forms.IntegerField(max_value=10000)
+    dim4 = forms.IntegerField(max_value=10000)
+    dim5 = forms.IntegerField(max_value=10000)
+    dim6 = forms.IntegerField(max_value=10000)
+    message = forms.CharField(widget=forms.Textarea, max_length=1000)
--- Pages/views.py
+++ Pages/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, HttpResponse
-from .forms import ContactForm
+from .forms import ContactForm, ContactForm_Adam
 import xlsxwriter
+import dropbox
+
+class TransferData: #létrehozom a TransferData osztályt
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to): #upload_file függvény létrehozása
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+                
+def main():
+    access_token = 'gAn1wHVkcYAAAAAAAAAAFskV2ZAJvluOD3PWg3JPO3nY_xU-Hm5uWBgiMTfsMT-Y'
+    transferData = TransferData(access_token)
+ 
+    file_from = 'D:/Dokumentumok/Biznisz/Code/Site/butor_sablon/butor_data_Adam.xlsx' # This is name of the file to be uploaded
+    file_to = '/butor_data_Adam.xlsx'  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+ 
+    # API v2
+    transferData.upload_file(file_from, file_to)
 
 def home_page(request):
     return render(request, 'home_page.html')
@@ -16,16 +37,17 @@ def about(request):
 
 def parameterek(request):
         if request.method == "GET":
-            form = ContactForm()
+            form_Adam = ContactForm_Adam()
         else:
-            form = ContactForm(request.POST)
-            if form.is_valid():                     # a ContactForm osztály minden eleme ki legyen töltve !!!!
-                dim1 = form.cleaned_data["dim1"]
-                dim2 = form.cleaned_data["dim2"]
-                dim3 = form.cleaned_data["dim3"]
-                dim4 = form.cleaned_data["dim4"]
-                dim5 = form.cleaned_data["dim5"]
-                dim6 = form.cleaned_data["dim6"]
+            form_Adam = ContactForm_Adam(request.POST)
+            if form_Adam.is_valid():                     # a ContactForm osztály minden eleme ki legyen töltve !!!!
+                dim1 = form_Adam.cleaned_data["dim1"]
+                dim2 = form_Adam.cleaned_data["dim2"]
+                dim3 = form_Adam.cleaned_data["dim3"]
+                dim4 = form_Adam.cleaned_data["dim4"]
+                dim5 = form_Adam.cleaned_data["dim5"]
+                dim6 = form_Adam.cleaned_data["dim6"]
+                message = form_Adam.cleaned_data["message"]
                 
                 butor_1_Adam = xlsxwriter.Workbook('butor_data_Adam.xlsx')
                 worksheet = butor_1_Adam.add_worksheet()
@@ -36,9 +58,10 @@ def parameterek(request):
                 worksheet.write(4, 0, dim5)
                 worksheet.write(5, 0, dim6)
                 butor_1_Adam.close()
+                main()
 
             #return redirect("home")
-        context = {"form": form}
+        context = {"form": form_Adam}
         return render(request, 'parameterek.html', context)
 
 def contact_user(request):
